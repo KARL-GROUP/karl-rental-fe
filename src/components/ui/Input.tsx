07caa40a5891cc5fd1a446520f9aad222495e2be
emilekamana <e.kamana@alustudent.com>
2023-02-02 11:25:55 +0200

    feat: input design

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,5 +1,15 @@
-const Input = () => {
-  return ( <></> );
+
+interface Props{
+  label: string
+}
+
+const Input = ({label}: Props) => {
+  return ( <>
+  <div className="relative z-0">
+    <input type="text" id="floating_standard" className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " />
+    <label htmlFor="floating_standard" className="absolute text-sm text-gray-300 dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Floating standard</label>
+</div>
+  </> );
 }
  
 export default Input;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import  Input  from '../components/ui/Input'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-    <h1 className='text-red-500'> Init</h1>  
+    <> 
+    <div className="bg-black h-[100vh] p-5">
+    <Input label='' />
+    </div>
       </>
   )
 }
