ea63c472932fcee8aedbfbea33d68ec6b43f64c5
Delyc <d.twizeyima@alustudent.com>
2023-02-21 11:01:26 +0200

    Fix: Fixing font import pathname

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import localFont from "@next/font/local";
 const roboto = localFont({
-  src: "../../public/Roboto/Roboto-regular.ttf",
+  src: "../../public/Roboto/Roboto-Regular.ttf",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
