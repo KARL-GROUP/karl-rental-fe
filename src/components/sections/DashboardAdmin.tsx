9d330572fbeb0251a06c2ba4c7d6be7eb1f067e9
Delyc <d.twizeyima@alustudent.com>
2023-03-02 23:34:29 +0200

    feat:dashboard admin

--- src/components/sections/DashboardAdmin.tsx
+++ src/components/sections/DashboardAdmin.tsx
@@ -1,8 +1,22 @@
 import CarCard from "../cards/CarCard";
 import { cars } from "@/data/cars";
+import Button from "../ui/Button";
 const DashboardAdmin = () => {
     return ( 
         <>
+        <div>
+            <p>All Cars</p>
+            <div>
+                <div>
+                <input placeholder="Search for a car"/>
+
+                </div>
+                <Button svg={<svg width="20" height="20" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M29.506 17.6466L17.006 17.6466L17.006 30.1466L12.8394 30.1466L12.8394 17.6466L0.339355 17.6466L0.339355 13.48L12.8394 13.48L12.8394 0.97998L17.006 0.97998L17.006 13.48L29.506 13.48L29.506 17.6466Z" fill="white"/>
+</svg>} text="Add new Car" 
+ />
+            </div>
+        </div>
         <div className="grid grid-cols-2 gap-10 w-4/5">
 
        
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,11 +2,12 @@ interface Props {
   text?: string,
   styles?: string,
   onClick?: () => void;
+  svg ?: any
 }
 
-const Button = ({ text, styles, onClick }: Props) => {
+const Button = ({ text, styles, onClick , svg}: Props) => {
   return (<>
-    <button onClick={onClick} className={`bg-[#3F3CD4] text-white text-lg rounded-lg px-5 py-3 ${styles}`}>{text}</button>
+    <button onClick={onClick} className={`bg-[#3F3CD4] text-white text-lg rounded-lg px-5 py-3 flex gap-5 justify-between items-center ${styles}`}>{svg}{text}</button>
   </>);
 }
 
