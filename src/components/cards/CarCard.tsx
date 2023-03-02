dc79b1946b9fecbf78733ef7c4844cc53dcfa654
Delyc <d.twizeyima@alustudent.com>
2023-03-02 23:41:16 +0200

    feat:dashboard admin

--- src/components/cards/CarCard.tsx
+++ src/components/cards/CarCard.tsx
@@ -5,7 +5,6 @@ import { Key } from "react";
 import Button from "../ui/Button";
 import SectionWrapper from "../Wrappers/sectionWrapper";
 import Profile from "../svgs/Profile";
-import CategoryIcon from "../svgs/CategoryIcon";
 import GearBox from '../../../public/assets/GearBox.png'
 
 interface Props {
@@ -21,20 +20,25 @@ const CarCard = ({ image, carName, price, category, seats }: Props) => {
     <>
 
 
-      <div className=" rounded-[16px] bg-white py-5 px-[30px] flex flex-col gap-5">
-      <p className="text-lg">{carName}</p>
-<p>Category category category</p>
-        <Image className="w-full object-fill rounded-t-[16px] " src={image} alt="car" width={100} height={100} />
+      <div className="shadow rounded-[16px] bg-white py-5 px-[30px] flex flex-col gap-5">
+        <p className="text-lg">{carName}</p>
+        <p>Category category category</p>
+        <div className="relative w-full h-[30vh]">
+          <Image className=" absolute object-fill rounded-t-[16px] " src={image} alt="car" layout="fill" />
+
+
+        </div>
         <div className="flex justify-between gap-2">
           <div className="flex items-center gap-2">
             <Profile />
-          <p className="mt-1">{seats}</p>
+            <p className="mt-1">{seats}</p>
           </div>
           <div className="flex items-center gap-2">
+
             <Image src={GearBox} width={18} height={18} alt="Category" />
-          <p>{category}</p>
+            <p>{category}</p>
           </div>
-          
+
           <p className="">{price}</p>
 
 
