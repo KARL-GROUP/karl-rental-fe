54fd789efa867a735699bb43002c34ababa1bfc5
Delyc <d.twizeyima@alustudent.com>
2023-03-04 22:24:41 +0200

    Feat: General info admin page

--- src/components/admin/GeneralInfo.tsx
+++ src/components/admin/GeneralInfo.tsx
@@ -1,3 +1,5 @@
+import CarCard from "../cards/CarCard";
+import { cars } from "@/data/cars";
 const GeneralInfo = () => {
     const recentOrders = [
         {
@@ -55,6 +57,22 @@ const GeneralInfo = () => {
                     })}
                 </div>
             </section>
+            <section className=" mt-10">
+            <h1 className="mb-5">Customer Favorites</h1>
+            <div className="h-[35vh]  grid grid-cols-3 gap-10  overflow-scroll">
+
+
+                {cars.map((car, index) => {
+                    return (
+                        // <h1>hello</h1>
+                        <CarCard key={index} image={car.image} carName={car.carName} price={car.price} category={car.category} seats={car.seats} />
+
+
+                    )
+                })}
+            </div>
+            </section>
+
         </>
     );
 }
