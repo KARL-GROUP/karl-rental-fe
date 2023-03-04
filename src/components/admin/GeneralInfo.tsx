7a118dee61ec33e46c3eeff6dbadec05b924596e
Delyc <d.twizeyima@alustudent.com>
2023-03-04 22:09:03 +0200

    Feat: General info admin page

--- src/components/admin/GeneralInfo.tsx
+++ src/components/admin/GeneralInfo.tsx
@@ -1,11 +1,49 @@
 const GeneralInfo = () => {
-    const recentOrder =[
-        
+    const recentOrders =[
+        {
+            plate: "RAC00DA",
+            status: "Pending",
+            from : "01/01/2022",
+            to: "01/01/2022"
+        },
+        {
+            plate: "RAC00DA",
+            status: "Pending",
+            from : "01/01/2022",
+            to: "01/01/2022"
+        },
+        {
+            plate: "RAC00DA",
+            status: "Pending",
+            from : "01/01/2022",
+            to: "01/01/2022"
+        },
+        {
+            plate: "RAC00DA",
+            status: "Pending",
+            from : "01/01/2022",
+            to: "01/01/2022"
+        },
+        {
+            plate: "RAC00DA",
+            status: "Pending",
+            from : "01/01/2022",
+            to: "01/01/2022"
+        },
+        {
+            plate: "RAC00DA",
+            status: "Pending",
+            from : "01/01/2022",
+            to: "01/01/2022"
+        },
     ]
     return ( 
         <>
         <section>
             <p>Recent Orders</p>
+            <div>
+                {/* {recentOrders} */}
+            </div>
         </section>
         </>
      );
