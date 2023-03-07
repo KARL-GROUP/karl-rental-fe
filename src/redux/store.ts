0c630330aa1184aff80c3f2db762487926ee8a5e
Delyc <d.twizeyima@alustudent.com>
2023-03-07 23:31:27 +0200

    Feat: UPdating store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,12 @@
 import { apiSlice } from '@/services/authApiSlice'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import {persistReducer, persistStore} from 'redux-persist'
+import {persistReducer, persistStore,  FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,} from 'redux-persist'
 
 import storage from "redux-persist/lib/storage";
 
@@ -21,7 +26,11 @@ export const store= configureStore({
    reducer: persistedReducer,
 
    middleware: (getDefaultMiddleware) => 
-   getDefaultMiddleware().concat(apiSlice.middleware)
+   getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }).concat(apiSlice.middleware),
 })
 
 export const persistor = persistStore(store)
